8770e45481612d1bcc7a3df0e721513ad9468024
MaryamMarigold <MaryamMarigold@users.noreply.github.com>
2015-07-19 22:05:14 +0430

    Update featureExtraction.m

--- code/featureExtraction.m
+++ code/featureExtraction.m
@@ -1,5 +1,23 @@
-function [score] = featureExtraction(data, otherDomainData, k)
-[IDX,C] = kmeans(data, k,'start','sample', 'emptyaction','singleton');
-C(isnan(C)) = 0;
-score = findScore(otherDomainData, C);
+function [scores] = featureExtraction(data, otherDomainData, k, row, counts)
+[IDX, C] = kmeans(data, k,'start','sample', 'emptyaction','singleton');
+% [C, IDX] = vl_kmeans(data, k, 'Initialization', 'plusplus');
+[rows, ~] = find(isnan(C) == 1);
+C(rows, :) = [];
+[labels, decisionvalue] = findScore(otherDomainData, C, row);
+global predictedInteractionMatrix;
+if(row)
+    for i = 1: size(otherDomainData, 1)
+        for j = 1:k
+            indexes = find(IDX == j);
+            if(~isempty(indexes))
+                indexes = rem(indexes, counts);
+                indexes = indexes+1;
+                predictedInteractionMatrix(i, indexes) = max(labels((i-1)*k+j,1), 0);
+            end
+        end
+    end
+end
+for i = 1: size(otherDomainData, 1)
+    scores(i, :) = decisionvalue((i-1)*k+1:i*k, 1);
+end
 end
