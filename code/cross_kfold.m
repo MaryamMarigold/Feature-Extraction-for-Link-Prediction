38ef0f7e17c36cad4458b662d35d3537e86ff076
MaryamMarigold <MaryamMarigold@users.noreply.github.com>
2015-08-14 17:10:21 +0430

    Update cross_kfold.m

--- code/cross_kfold.m
+++ code/cross_kfold.m
@@ -1,5 +1,5 @@
 function [c, bestDegree] = cross_kfold(trainX,trainY,n_fold)
-degrees = [4, 6, 8];
+degrees = [6, 8];
 size_degree = numel(degrees);
 cv_error = zeros(1, size_degree);
 for i=1:size_degree
@@ -27,6 +27,7 @@ for i=1:size_degree
     %     end
     cv_error(i) = mean(cv_err_arr);
     cv_error(i) = sqrt(mean((newvalue-cv_testY).^2));
+    clear model1; clear cv_trainX; clear size_each_part; clear rand_map; clear cv_test_idx; clear cv_trainY; clear cv_testX; clear cv_testX; clear newvalue; clear cv_test_idx; clear cv_train_idx;
 end
 idxMin = 1;
 for i=2:size_degree
@@ -34,9 +35,10 @@ for i=2:size_degree
         idxMin =  i;
     end
 end
+ clear cv_err_arr;clear cv_error;
+ cv_error = zeros(1, 4);
 bestDegree=degrees(idxMin);
-clear cv_error;
-for i=3:-1:-3
+for i=1:-1:-2
     cv_err_arr = zeros(1, n_fold);
     size_each_part = floor(size(trainX,1)/n_fold);
     rand_map = randperm(size(trainX,1));
@@ -57,15 +59,18 @@ for i=3:-1:-3
     str = strcat(str, ' ', sprintfc(' ,-c %d', 10^i));
     model1 = svmtrain(cv_trainY, cv_trainX, str(1, 1));
     [newvalue, ~, ~] = svmpredict(cv_testY, cv_testX, model1);
-    cv_err_arr(fold)=sqrt(mean((newvalue-cv_testY).^2));
-    cv_error(i+6) = mean(cv_err_arr);
-    cv_error(i+6) = sqrt(mean((newvalue-cv_testY).^2));
+    cv_err_arr(fold, 1)=sqrt(mean((newvalue-cv_testY).^2));
+    cv_error(i+3, 1) = mean(cv_err_arr);
+    cv_error(i+3, 1) = sqrt(mean((newvalue-cv_testY).^2));
+    clear model1; clear cv_trainX; clear size_each_part; clear rand_map; clear cv_test_idx; clear cv_trainY; clear cv_testX; clear cv_testX; clear newvalue; clear cv_test_idx; clear cv_train_idx;
 end
 idxMin = 1;
-for i=1:7
+for i=1:4
     if(cv_error(i)<=cv_error(idxMin))
         idxMin =  i;
     end
 end
-c = 10^(idxMin-4);
+c = 10^(idxMin-3);
+clear cv_err_arr;clear cv_error;
 end
+ 
