5f62f19ea4881ba1080f2ea27e726cf0d5638255
MaryamMarigold <MaryamMarigold@users.noreply.github.com>
2015-08-14 17:12:17 +0430

    Update testFeatureExtraction.m

--- code/testFeatureExtraction.m
+++ code/testFeatureExtraction.m
@@ -1,25 +1,24 @@
 function [predictedInteractionMatrixTest] = testFeatureExtraction(data, otherDomainData, k, count1, count2)
-diff = 1;
-if(size(data, 1) <k)
-    diff = k-size(data, 1);
-    data = [zeros(diff, size(data, 2)); data];
-end
 [IDX, C] = kmeans(data, k, 'start', 'sample', 'emptyaction','drop');
 % [C, IDX] = vl_kmeans(data, k, 'Initialization', 'plusplus');
- [row, ~] = find(isnan(C) == 1);
+[row, ~] = find(isnan(C) == 1);
 C(row, :) = [];
 [labels, ~] = findScore(otherDomainData, C, 1);
 distinctCrulters = unique(IDX);
 k = size(distinctCrulters, 1);
-predictedInteractionMatrixTest = zeros(count1, count2);
+predictedInteractionMatrixTest = zeros(count2, count1);
 for i = 1: size(otherDomainData, 1)
-    for j = 1:k
-        indexes = find(find(IDX ==distinctCrulters(j, 1) ) >= diff);
-        if(~isempty(indexes))
-            indexes = rem(indexes, count1);
-            indexes = indexes+1;
-            predictedInteractionMatrixTest(i, indexes) = max(labels((i-1)*k+j,1), 0);
+    iperim = rem(i, count2);
+        if(iperim == 0)
+            iperim = count2;
         end
+    for j = 1:k
+        indexes = find(IDX == distinctCrulters(j, 1));
+        indexes = rem(indexes, count1);
+        indexes(find(indexes == 0)) = count1;
+        predictedInteractionMatrixTest(iperim, indexes) = max(max(labels((i-1)*k+j,1), 0),...
+            predictedInteractionMatrixTest(iperim, indexes));
     end
 end
+clear IDX; clear C; clear indexes; clear distinctCrulters;
 end
