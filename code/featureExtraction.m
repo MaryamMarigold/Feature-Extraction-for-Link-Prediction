be80281e551c347e2d9e8559fdf38d206b7edd6f
MaryamMarigold <MaryamMarigold@users.noreply.github.com>
2015-08-14 17:11:13 +0430

    Update featureExtraction.m

--- code/featureExtraction.m
+++ code/featureExtraction.m
@@ -1,23 +1,51 @@
-function [scores] = featureExtraction(data, otherDomainData, k, row, counts)
+function [scores] = featureExtraction(data, otherDomainData, k, row, count1, count2)
 [IDX, C] = kmeans(data, k,'start','sample', 'emptyaction','singleton');
 % [C, IDX] = vl_kmeans(data, k, 'Initialization', 'plusplus');
 [rows, ~] = find(isnan(C) == 1);
 C(rows, :) = [];
 [labels, decisionvalue] = findScore(otherDomainData, C, row);
+% labels = ones(size(otherDomainData, 1)*size(C, 1), 1);
 global predictedInteractionMatrix;
+distinctCrulters = unique(IDX);
+kp = size(distinctCrulters, 1);
 if(row)
     for i = 1: size(otherDomainData, 1)
-        for j = 1:k
-            indexes = find(IDX == j);
+        iperim = rem(i, count2);
+        if(iperim == 0)
+            iperim = count2;
+        end
+        for j = 1:kp
+            indexes = find(IDX == distinctCrulters(j, 1));
             if(~isempty(indexes))
-                indexes = rem(indexes, counts);
-                indexes = indexes+1;
-                predictedInteractionMatrix(i, indexes) = max(labels((i-1)*k+j,1), 0);
+                indexes = rem(indexes, count1);
+                indexes(find(indexes == 0)) = count1;
+                predictedInteractionMatrix(iperim, indexes) = max(max(labels((i-1)*k+j,1), 0), predictedInteractionMatrix(iperim, indexes));
+            end
+        end
+    end
+else
+    for i = 1: size(otherDomainData, 1)
+        iperim = rem(i, count2);
+        if(iperim == 0)
+            iperim = count2;
+        end
+        for j = 1:kp
+            indexes = find(IDX == distinctCrulters(j, 1));
+            if(~isempty(indexes))
+                indexes = rem(indexes, count1);
+                indexes(find(indexes == 0)) = count1;
+                predictedInteractionMatrix(indexes, iperim) =max(max(labels((i-1)*k+j,1), 0), predictedInteractionMatrix(indexes, iperim));
             end
         end
     end
 end
+scores = zeros(count2, k);
 for i = 1: size(otherDomainData, 1)
-    scores(i, :) = decisionvalue((i-1)*k+1:i*k, 1);
+        iperim = rem(i, count2);
+        if(iperim == 0)
+            iperim = count2;
+        end
+    scores(iperim, :) = decisionvalue((i-1)*k+1:i*k, 1)';
 end
+clear IDX; clear C; clear indexes; clear distinctCrulters; clear rows; clear labels; clear decisionvalueV; clear iperim;
 end
