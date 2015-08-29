2c46bef1d7ef1345183d4501bd474b46b185fe4b
MaryamMarigold <MaryamMarigold@users.noreply.github.com>
2015-08-29 23:44:18 +0430

    Update calcPrecRecall.m

--- code/calcPrecRecall.m
+++ code/calcPrecRecall.m
@@ -1,8 +1,11 @@
-function [prec, recall] = calcPrecRecall(labels, score)
-summ = labels+score;
-diff = labels - score;
-tp = size(find(summ == 2), 1);
-prec = tp/size(find(labels == 1), 1);
-recall = tp/size(find(diff == 0), 1);
+function [prec, recall] = calcPrecRecall(maskData, labels)
+global predictedInteractionMatrix;
+summ = labels+predictedInteractionMatrix;
+diff = labels - predictedInteractionMatrix;
+mask = false(250, 315);
+mask((maskData(:, 2)-1)*(size(mask, 1))+ maskData(:, 1)) = true;
+tp = size(find((mask(summ == 2) == 1)), 1);
+prec = tp/size(find(mask(labels == 1) == 1), 1);
+recall = tp/size(find(mask(diff == 0) == 1), 1);
 clear summ;clear diff; clear tp;
 end
